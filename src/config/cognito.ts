dc3e6b3b0701441662e489514b433593d8ee4bb2
Nerimas Petraitis <nerimas86@gmail.com>
2023-02-09 10:38:49 +0000

    update on Cognito.ts

--- src/config/cognito.ts
+++ src/config/cognito.ts
@@ -4,5 +4,5 @@ export let cognitoConfig = {
         ClientId: 'a3us4rrhlvcso6eid68c5ue91',
         region: 'us-east-1'
     },
-    identityPoolId: 'us-east-1:cf1c1866-5331-409a-808e-dfa1925bf9fc'
+    identityPoolId: 'us-east-1:3fede4b4-d65e-49e1-98d4-b98fe58c6989'
 };
