2769af9a0f25df95600d943601c00a0ad7d01803
AnxiousUK <72379849+AnxiousUK@users.noreply.github.com>
2023-02-09 10:31:37 +0000

    Client ID updated, taken from "App clients and analytics"

--- src/config/cognito.ts
+++ src/config/cognito.ts
@@ -1,7 +1,7 @@
 export let cognitoConfig = {
     userPool: {
         UserPoolId: 'us-east-1_d9PhIMDar',
-        ClientId: 'a3us4rrhlvcso6eid68c5ue91',
+        ClientId: '4ar8ugqu7elcu1dp3ergi4ndjb',
         region: 'us-east-1'
     },
     identityPoolId: 'us-east-1:cf1c1866-5331-409a-808e-dfa1925bf9fc'
